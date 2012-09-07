f94628653c212b60dc30a57eddda726bead3ba76
jdmellor <mellor.jonathan@gmail.com>
2012-09-07 22:14:21 +0000

    added android.js and other pages

--- android.js
+++ android.js
@@ -1,11 +1,16 @@
-if (window.innerWidth && window.innerWidth <= 480) { 
-    $(document).ready(function(){ 
-        $('#header ul').addClass('hide'); 
-        $('#header').append('<div class="leftButton" 
-            onclick="toggleMenu()">Menu</div>'); 
+$(document).ready(function(){ 
+    loadPage();
+});
+function loadPage(url) {
+   if (url == undefined) {
+      $('#container').load('index.html #header ul', hijackLinks);
+   } else {
+      $('#container').load(url + ' #content', hijackLinks);
+   }
+}
+function hijackLinks() {
+    $('#container a').click(function(e){
+        e.preventDefault();
+        loadPage(e.target.href);
     });
-    function toggleMenu() { 
-        $('#header ul').toggleClass('hide'); 
-        $('#header .leftButton').toggleClass('pressed'); 
-    }
 }
